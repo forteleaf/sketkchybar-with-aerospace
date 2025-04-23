8e0091d277f1cb62a2008ccfe86c2caaf9bcfa40
forteleaf <forteleaf@hotmail.com>
2025-04-23 09:16:12 +0900

    change sf pro -> nerd font icon

--- sketchybar/helpers/default_font.lua
+++ sketchybar/helpers/default_font.lua
@@ -1,13 +1,13 @@
 return {
-  text = "SF Pro", -- Used for text
-  numbers = "SF Mono", -- Used for numbers
+	text = "ZedMono Nerd Font", -- Used for text
+	numbers = "SF Mono", -- Used for numbers
 
-  -- Unified font style map
-  style_map = {
-    ["Regular"] = "Regular",
-    ["Semibold"] = "Semibold",
-    ["Bold"] = "Bold",
-    ["Heavy"] = "Heavy",
-    ["Black"] = "Black",
-  }
+	-- Unified font style map
+	style_map = {
+		["Regular"] = "Regular",
+		["Semibold"] = "Semibold",
+		["Bold"] = "Bold",
+		["Heavy"] = "Heavy",
+		["Black"] = "Black",
+	},
 }
--- sketchybar/settings.lua
+++ sketchybar/settings.lua
@@ -1,22 +1,23 @@
 return {
-  paddings = 3,
-  group_paddings = 5,
+	paddings = 3,
+	group_paddings = 5,
 
-  icons = "sf-symbols", -- alternatively available: NerdFont
+	-- icons = "sf-symbols", -- alternatively available: NerdFont
+	icons = "NerdFont", -- alternatively available: NerdFont
 
-  -- This is a font configuration for SF Pro and SF Mono (installed manually)
-  font = require("helpers.default_font"),
+	-- This is a font configuration for SF Pro and SF Mono (installed manually)
+	font = require("helpers.default_font"),
 
-  -- Alternatively, this is a font config for JetBrainsMono Nerd Font
-  -- font = {
-  --   text = "JetBrainsMono Nerd Font", -- Used for text
-  --   numbers = "JetBrainsMono Nerd Font", -- Used for numbers
-  --   style_map = {
-  --     ["Regular"] = "Regular",
-  --     ["Semibold"] = "Medium",
-  --     ["Bold"] = "SemiBold",
-  --     ["Heavy"] = "Bold",
-  --     ["Black"] = "ExtraBold",
-  --   },
-  -- },
+	-- Alternatively, this is a font config for JetBrainsMono Nerd Font
+	-- font = {
+	--   text = "JetBrainsMono Nerd Font", -- Used for text
+	--   numbers = "JetBrainsMono Nerd Font", -- Used for numbers
+	--   style_map = {
+	--     ["Regular"] = "Regular",
+	--     ["Semibold"] = "Medium",
+	--     ["Bold"] = "SemiBold",
+	--     ["Heavy"] = "Bold",
+	--     ["Black"] = "ExtraBold",
+	--   },
+	-- },
 }
